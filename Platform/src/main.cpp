b61c3d71f4355e5aff265d93091dbf18b5728461
tilakchad111 <tilakchad111@gmail.com>
2022-07-13 23:39:43 +0545

    CMake configured, works on msvc too

--- Platform/src/main.cpp
+++ Platform/src/main.cpp
@@ -1,7 +1,8 @@
 #include <format>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    std::cout << std:format("{}","Hello Universe") << std::endl;
+    std::cout << std::format("{}","Hello Universe") << std::endl;
     return 0;
 }
