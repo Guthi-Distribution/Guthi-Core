82d6faca4a81095ccfd333a279d967b980410bdb
Sanskar Amgain <sanskaramgain@gmail.com>
2022-09-28 16:57:08 +0545

    POSIX shm

--- src/event/event.cpp
+++ src/event/event.cpp
@@ -1,4 +1,4 @@
-#include "event/event.h"
+#include "./event.h"
 
 namespace Guthi
 {
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,9 @@
 #include <shared_memory/shm.h>
 
 int main() {
-    SharedMemory memory("s_mem", O_CREAT, S_IWUSR | S_IWRITE);
+    SharedMemory memory("s_mem", O_CREAT, 0666);
+    printf("%d\n", S_IWUSR | S_IWRITE);
     memory.write_data("Hello there\n");
+    getchar();
+    printf("%s\n", memory.read_data());
 }
