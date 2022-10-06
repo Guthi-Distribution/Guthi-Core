95f6c874a5fed5976ce2819c92ef40dedfbc7091
Sanskar Amgain <sanskaramgain@gmail.com>
2022-10-06 17:33:55 +0545

    SHM added

--- src/shared_memory/shm.h
+++ src/shared_memory/shm.h
@@ -8,64 +8,61 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <inttypes.h>
 
 #ifndef SIZE_SHARED_MEM
-#define SIZE_SHARED_MEM 4096
+    #define SIZE_SHARED_MEM 4096
+#else
+    static_assert SIZE_SHARED_MEM < 65536, "Size of set shared mem greater than 65536 is not allowed";
 #endif
 
 #ifdef _MSC_VER
 TODO: Windows version
 #endif
 
+#define SHM_KEY 69
+
+struct ShmSegment {
+    uint16_t count;
+    char buff[SIZE_SHARED_MEM];
+};
+
 #if defined(__gnu_linux__) || defined(__linux__) || defined(linux) || defined(__linux)
 struct SharedMemory {
-    int fd; // file descriptor
-    std::string_view name; // name of the shared_memory, ofc
-    char *memory; // memory which will be mapped to shared memory
+    int id; // file descriptor
+    ShmSegment *memory; // memory which will be mapped to shared memory
     sem_t sem;
     key_t key;
 
-    SharedMemory(const char* _name, int flag, int mode): name(_name) {
-        fd = shm_open(_name, flag | O_RDWR, mode);
-        if (fd < 0) {
-            if (errno == EACCES) {
-                printf("Permission to shm_unlink() the shared memory object was denied.\n");
-            } else if (errno == EACCES) {
-                printf("Permission was denied to shm_open() name in the specified mode, or O_TRUNC was specified and the caller does not have write permission on the object.\n");
-            } else if (errno == EEXIST) {
-                printf("Both O_CREAT and O_EXCL were specified to shm_open() and the shared memory object specified by name already exists.\n");
-            } else if (errno == EINVAL) {
-                printf("The name argument to shm_open() was invalid\n");
-            } else if (errno == ENOENT) {
-                printf("An attempt was made to shm_open() a name that did not exist, and O_CREAT was not specified\n");
-            }
+    SharedMemory(const char* _name, int flag, int mode) {
+        id = shmget(SHM_KEY, sizeof(ShmSegment), IPC_CREAT | (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP));
+        if (id == -1) {
+            perror("Shared Memory Creation error");
+            if (errno == EACCES) printf("A shared  memory identifier exists for key but operation permission as specified by the low-order nine bits of shmflg would not be granted.\n");
+            if (errno == EEXIST) printf("A shared memory identifier exists for the argument key but (shmflg &IPC_CREAT) &&(shmflg &IPC_EXCL) is non-zero.\n");
+            if (errno == EINVAL) printf("A shared memory segment is to be created and the value of size is less than the system-imposed minimum or greater than the system-imposed maximum.\n");
+            if (errno == EINVAL) printf("No shared memory segment is to be created and a shared memory segment exists for key but the size of the segment associated with it is less than size.\n");
+            if (errno == ENOENT) printf("A shared memory identifier does not exist for the argument key and (shmflg &IPC_CREAT) is 0.\n");
+            if (errno == ENOMEM) printf("A shared memory identifier and associated shared memory segment are to be created, but the amount of available physical memory is not sufficient to fill the request.\n");
+            if (errno == ENOSPC) printf("A shared memory identifier is to be created, but the system-imposed limit on the maximum number of allowed shared memory identifiers system-wide would be exceeded.\n");
             exit(-1);
         }
-        ftruncate(fd, SIZE_SHARED_MEM);
-        memory = (char *)mmap(NULL, SIZE_SHARED_MEM, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+        memory = (ShmSegment *)shmat(id, NULL, 0);   
+        if (memory == NULL) {
+            perror("Memory attach error\n");
+        }
     }
 
     void write_data(const char *data, int position = 0) {
-        if (position >= SIZE_SHARED_MEM) {
-            //TODO: Error handling
-            return; 
-        }
-        flock(fd, LOCK_EX);
-        sprintf(memory + position, data);
-        flock(fd, LOCK_UN);
+        
     }
 
     const char* read_data(int position = 0) {
-        if (position >= SIZE_SHARED_MEM) {
-            //TODO: Error handling
-            return nullptr;
-        }
-        return (const char *)(memory + position);
     }
 
     ~ SharedMemory() {
-        shm_unlink(name.data());
-        munmap(memory, SIZE_SHARED_MEM);
+        shmdt((const void *)memory);
+        
     }
 };
 #endif
