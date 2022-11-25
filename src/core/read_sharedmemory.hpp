0f75d526dd1f5f870e69c6a0328f5f47a6a366ab
Sanskar Amgain <sanskar.amgain@lisnepal.com.np>
2022-11-25 18:02:31 +0545

    Filesystem communication added

--- src/core/read_sharedmemory.hpp
+++ src/core/read_sharedmemory.hpp
@@ -1,5 +1,7 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
+#include <error.h>
 #include <iostream>
 #include <shared_memory/shm.h>
 #include <shared_memory/semaphore.hpp>
@@ -11,23 +13,54 @@ struct ThreadInformation {
     SharedMemory shm;
     Semaphore sem;
 };
-
 static ThreadInformation info;
-#if defined(__gnu_linux__) || defined(__linux__) || defined(linux) || defined(__linux)
+
+
+typedef uint8_t MESSAGE_TYPE;
+typedef uint8_t EVENT_TYPE;
+
+// various types of message 
+#define MESSAGE_EVENT '0'
+#define MESSAGE_FILESYSTEM '1'
+
+struct SharedMemoryInformation {
+    MESSAGE_TYPE message_type; 
+    char *message = nullptr;
+    EVENT_TYPE event_type;
+};
+
+static SharedMemoryInformation parse_shared_memory(char *ptr, int count = 0) {
+    if (count < 1) {
+        return {};
+    }
+    SharedMemoryInformation info;
+    if (ptr[1] == MESSAGE_EVENT) {
+        
+    } else if (ptr[1] == MESSAGE_FILESYSTEM) {
+        
+    }
+    info.message_type = ptr[1];
+    info.message = ptr + 2;
+
+
+    return info;
+}
+
 void *read_shm(void* gfs) {
     while (true) {
         int count = info.shm.shm_segment->count;
         char *data = info.shm.shm_segment->buff;
         if (data[0] == '1') {
-            std::vector<uint8_t> serialized;
-            for (int i = 1; i <= count; i++) {
-                serialized.push_back((uint8_t)data[i]);
+            SharedMemoryInformation info = parse_shared_memory(data, count);
+            if (info.message_type == MESSAGE_FILESYSTEM) {
+                std::vector<uint8_t> serialized;
+                for (int i = 1; i <= count; i++) {
+                    serialized.push_back((uint8_t)data[i]);
+                }
+                FileSystem::FileContent deserialized;
+                assert(FileSystem::NetworkFS::DeserializeToFileContent(serialized, deserialized));
+                std::cout << deserialized << std::endl;
             }
-            FileSystem::FileContent deserialized;
-            assert(FileSystem::NetworkFS::DeserializeToFileContent(serialized, deserialized));
-            std::cout << deserialized << std::endl;
         }
     }
 }
-
-#endif
